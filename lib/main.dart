07d261fc4ddc07189c214b7601aa594519ed5876
renga <abi129.off@gmail.com>
2025-04-05 14:45:42 +0530

    Height issues are fixed and set as dynamic

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class _ImageCarouselScreenState extends State<ImageCarouselScreen> {
     },
   ];
 
-  int _currentIndex = 0;
+  int _currentIndex = 1;
   Color myColor = Color(0xFF36B273);
 
   @override
@@ -61,132 +61,144 @@ class _ImageCarouselScreenState extends State<ImageCarouselScreen> {
     double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: myColor,
-      body: Stack(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-              padding: EdgeInsets.all(20),
-              height: screenHeight * 0.5,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(30),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black12,
-                    offset: Offset(-3, 10),
-                    blurRadius: 5,
-                    spreadRadius: 2,
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.9,
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+                    padding: EdgeInsets.all(20),
+                    height: screenHeight * 0.5,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          offset: Offset(-3, 10),
+                          blurRadius: 5,
+                          spreadRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: CarouselSlider.builder(
+                      carouselController: carouselController!,
+                      options: CarouselOptions(
+                        scrollDirection: Axis.vertical,
+                        enlargeFactor: 1,
+                        viewportFraction: 1,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            _currentIndex = index;
+                            carouselController!.animateToPage(index);
+                          });
+                        },
+                      ),
+                      itemCount: details.length,
+                      itemBuilder: (context, index, realIndex) {
+                        return Column(
+                          spacing: 20,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "\$ ${details[index]['price']!}",
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              details[index]['title']!,
+                              style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              details[index]['desc']!,
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: Size(double.infinity, 50),
+                                backgroundColor: Colors.black,
+                                elevation: 5, // elevation
+                              ),
+                              onPressed: () {},
+                              child: Text(
+                                "Get it!",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
                   ),
-                ],
-              ),
-              child: CarouselSlider.builder(
-                carouselController: carouselController!,
-                options: CarouselOptions(
-                  scrollDirection: Axis.vertical,
-                  enlargeFactor: 1,
-                  viewportFraction: 1,
-                  onPageChanged: (index, reason) {
-                    setState(() {
-                      _currentIndex = index;
-                      carouselController!.animateToPage(index);
-                    });
+                ),
+                CarouselSlider.builder(
+                  carouselController: carouselController!,
+                  options: CarouselOptions(
+                    height: 400.0,
+                    enlargeCenterPage: true,
+                    viewportFraction: 0.45,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        _currentIndex = index;
+                        carouselController!.animateToPage(index);
+                      });
+                    },
+                  ),
+                  itemCount: imagePaths.length,
+                  itemBuilder: (context, index, realIndex) {
+                    bool isSelected = index == _currentIndex;
+                    double scale = isSelected ? 1 : 0.5; // Enlarge center item
+
+                    return TweenAnimationBuilder(
+                      duration: Duration(milliseconds: 300),
+                      tween: Tween<double>(begin: scale, end: scale),
+                      builder: (context, value, child) {
+                        return Transform.scale(
+                          scale: value,
+                          child: Image.asset(
+                            imagePaths[index],
+                            fit: BoxFit.fitHeight,
+                            width: double.infinity,
+                          ),
+                        );
+                      },
+                    );
                   },
                 ),
-                itemCount: details.length,
-                itemBuilder: (context, index, realIndex) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 0),
-                      Text(
-                        "\$ ${details[index]['price']!}",
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        details[index]['title']!,
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        details[index]['desc']!,
-                        style: TextStyle(fontSize: 15, color: Colors.grey),
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size(double.infinity, 50),
-                          backgroundColor: Colors.black,
-                          elevation: 5, // elevation
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          "Get it!",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ],
-                  );
-                },
-              ),
-            ),
-          ),
-          CarouselSlider.builder(
-            carouselController: carouselController!,
-            options: CarouselOptions(
-              height: 400.0,
-              enlargeCenterPage: true,
-              viewportFraction: 0.45,
-              onPageChanged: (index, reason) {
-                setState(() {
-                  _currentIndex = index;
-                  carouselController!.animateToPage(index);
-                });
-              },
-            ),
-            itemCount: imagePaths.length,
-            itemBuilder: (context, index, realIndex) {
-              bool isSelected = index == _currentIndex;
-              double scale = isSelected ? 1 : 0.8; // Enlarge center item
-
-              return TweenAnimationBuilder(
-                duration: Duration(milliseconds: 300),
-                tween: Tween<double>(begin: scale, end: scale),
-                builder: (context, value, child) {
-                  return Transform.scale(
-                    scale: value,
-                    child: Image.asset(
-                      imagePaths[index],
-                      fit: BoxFit.fitHeight,
-                      width: double.infinity,
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: ClipRRect(
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                      child: SizedBox(height: 300, width: 100),
                     ),
-                  );
-                },
-              );
-            },
-          ),
-          Align(
-            alignment: Alignment.topLeft,
-            child: ClipRRect(
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                child: SizedBox(height: 330, width: 100),
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.topRight,
-            child: ClipRRect(
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                child: SizedBox(height: 330, width: 100),
-              ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: ClipRRect(
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                      child: SizedBox(height: 300, width: 100),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
